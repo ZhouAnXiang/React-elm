f452ea71948e1b963fd964dcdf15b1f116fdf5df
ZhouAnXiang <2575610534@qq.com>
2018-09-20 13:39:34 +0800

    登录页开发

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  withRouter
+} from "react-router-dom";
 import './App.css';
+import './assets/css/login.css';
+
+
+//页面路由
+import login from './pages/login' // 登录页
+import wrapper from './pages/navWrap' // 导航页
+import HomeOne from './pages/home/home1' // 首页一
+import HomeTwo from './pages/home/home2' // 首页二
+import HomeThree from './pages/home/home3' // 首页二
+
+const router = [
+  {
+    path: "/",
+    exact: true,
+    component: login
+  },
+  {
+    path: "/wrapper",
+    component: wrapper,
+    childRoutes: [
+      {
+        path: "/HomeOne",
+        sidebar: HomeOne,
+        component: HomeOne
+      },
+      {
+        path: "/HomeTwo",
+        sidebar: HomeTwo,
+        component: HomeTwo
+      },
+      {
+        path: "/HomeThree",
+        sidebar: HomeThree,
+        component: HomeThree
+      }
+    ]
+  }
+];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="Contianer">
+          <Route exact path="/" component={login} />
+          <Route path="/wrapper" component={wrapper} />
+        </div>
+      </Router>
     );
   }
 }
